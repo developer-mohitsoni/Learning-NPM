8bb083c8220bc077b4baec8de9361d80fd13514b
Mohit Soni <developer_mohitsoni@outlook.com>
2024-02-11 20:54:18 +0530

    install new package

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
+import Quote from 'inspirational-quotes'
 
 // fetch data using axios
 
 axios.get("https://jsonplaceholder.typicode.com/todos/1")
     .then((response)=>{
-        console.log(response.data);
+        console.log(response.data);  // Later Executed
     });
+
+    console.log(Quote.getQuote());  // Executed First
